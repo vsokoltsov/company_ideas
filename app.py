4ae9f3ff258648470d10ea10e4e7579ba8566318
Vadim Sokoltsov <vforvad@gmail.com>
2018-07-07 19:55:08 +0300

    add example file and fix app module

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ async def hello(request):
     return web.Response(text="Hello, world")
 
 
-app = web.Application()
-app.add_routes([web.get('/', hello)])
-web.run_app(app)
+def start(args):
+    app = web.Application()
+    app.add_routes([web.get('/', hello)])
+    web.run_app(app,port=8000)
