f8e8f24614b2161cb974eac571b545f2d3ae4596
Vadim Sokoltsov <vforvad@gmail.com>
2018-07-15 18:52:00 +0300

    implement creation of the new users

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from aiohttp import web
+import pymongo
+from db import dbclient
+import bcrypt
 import ipdb
 
 async def hello(request):
+    users = dbclient.users.find()
+    collection = await users.to_list(length=100)
     return web.Response(text="Hello, world")
 
 async def sign_in(request):
@@ -10,7 +15,17 @@ async def sign_in(request):
 
 async def sign_up(request):
     data = await request.json()
-    return web.json_response(data)
+    salt = bcrypt.gensalt()
+    password_digest = bcrypt.hashpw(data.get('password'), salt)
+    user = {
+        'email': data.get('email'),
+        'password_digest': password_digest
+    }
+    try:
+        result = await dbclient.users.insert_one(user)
+        return web.json_response(result)
+    except pymongo.errors.DuplicateKeyError as e:
+        return web.json_response(e.args[0])
 
 async def current_user(request):
     return web.json_response(request)
