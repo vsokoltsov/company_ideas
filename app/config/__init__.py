d2ef186406d400e337aacd254f1fabfd2a3fe8fe
Vadim Sokoltsov <vforvad@gmail.com>
2018-07-22 00:06:01 +0300

    change config classes

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -4,13 +4,14 @@ from app.utils import get_env_variable
 from .development import ConfigDevelopment
 from .test import ConfigTest
 
+APP_ENV = 'APP_ENV'
 DEVELOPMENT = 'development'
 TEST = 'test'
 
 
 def get_actual_environment():
     """Return config class based on environment variable."""
-    env = os.environ.get('APP_ENV', DEVELOPMENT)
+    env = os.environ.get(APP_ENV, DEVELOPMENT)
     if env == TEST:
         return ConfigTest
     else:
