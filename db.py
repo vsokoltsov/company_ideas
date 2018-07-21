402fb2d5014623aa269e81c4909f51a7e377e0e2
Vadim Sokoltsov <vforvad@gmail.com>
2018-07-21 21:54:34 +0300

    change database file

--- db.py
+++ db.py
@@ -1,7 +1,9 @@
 import asyncio
 import motor.motor_asyncio
 
-# TODO figure out how to implement creation database for the test and dev environments
+from app.config import get_actual_environment
+
+env = get_actual_environment()
 
 async def create_user_unique_index(client):
     return await client.users.create_index('email', unique=True)
@@ -12,7 +14,9 @@ async def create_user_collection(client):
     except:
         pass
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://root:root@mongo:27017')
-dbclient = client.client_ideas
+client = motor.motor_asyncio.AsyncIOMotorClient(
+    'mongodb://root:root@mongo:27017'
+)
+dbclient = client[env.COLLECTION]
 asyncio.ensure_future(create_user_collection(dbclient))
 asyncio.ensure_future(create_user_unique_index(dbclient))
